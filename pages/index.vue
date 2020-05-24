c69cd2ffb751e44083417476f2d648f6f12f2c37
elpeyotl <ch.perez@mac.com>
2020-05-24 22:02:36 +0200

    push

--- pages/index.vue
+++ pages/index.vue
@@ -3,25 +3,62 @@
     <div class="content-left">
         <div class="container">
           <div class="content__heading">
-            <h1>Bildhauerei<br>Grabmale<br>Workshops</h1>
+            <h1 v-html="$md.render(pageContent.title)"></h1>
           </div>
 
-           <div class="content__element">
+           <div class="content__element" v-html="$md.render(pageContent.address)">
+            </div>
+
+           <div class="content__element" v-html="$md.render(pageContent.intro)">
+            </div>
+        </div>
+    </div>
+
+     <div class="content-right">
+        <div class="container">
+
+            <Swiper />
+
+            <div class="img-carousel">
+              hoho
+            </div>
+            <div id="slider">
+               bla
             </div>
         </div>
 
-        {{pageContent}}
+
+        <div class="container">
+            <h2>Angebot</h2>
+            <div v-html="$md.render(pageContent.angebot)" class="mb-12"></div>
+            <div class="tab">
+                <h4 class="tab__heading tab--active">
+                    <a href="#">News</a>
+                </h4>
+                <ul class="no-bullets">
+                    <li>
+                        <a href="#">
+                        <div class="small">postdate</div>
+                        posttitle
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import Swiper from '~/components/swiper'
 
 export default {
+  components: {
+    Swiper
+  },
   computed: {
     pageContent () {
-      return  this.$store.getters['content/retrievePage']('home')
+      return this.$store.getters.retrievePage('home')
     }
   },
 };
--- store/index.js
+++ store/index.js
@@ -1,19 +1,58 @@
 export const state = () => ({
-  sideBarOpen: false
+    sideBarOpen: false,
+    blogPosts: [],
+    pages: []
 });
 
 export const mutations = {
-  toggleSideBar(state) {
+    toggleSideBar(state) {
     state.sideBarOpen = !state.sideBarOpen;
-  },
-};
-
-export const actions = {
-    toggleSidebar({ commit }) {
-      commit("toggleSideBar");
+    },
+    setBlogPosts(state, list) {
+    state.blogPosts = list;
+    },
+    setPages(state, list) {
+    state.pages = list;
     }
 };
 
-export const getters = {
+export const actions = {
+         toggleSidebar({ commit }) {
+           commit("toggleSideBar");
+         },
+         async nuxtServerInit({ commit }) {
+           // Get Blog posts
+           let blogFiles = await require.context(
+             "~/assets/content/blog/",
+             false,
+             /\.json$/
+           );
+           let blogPosts = blogFiles.keys().map(key => {
+             let res = blogFiles(key);
+             res.slug = key.slice(2, -5);
+             return res;
+           });
+           console.log(blogPosts)
+           await commit("setBlogPosts", blogPosts);
 
+           //Get all Pages
+           let pageFiles = await require.context(
+             "~/assets/content/pages/",
+             false,
+             /\.json$/
+           );
+           let pages = pageFiles.keys().map(key => {
+             let res = pageFiles(key);
+             res.slug = key.slice(2, -5);
+             return res;
+           });
+           console.log(pages)
+           await commit("setPages", pages);
+         }
+       };
+
+export const getters = {
+    retrievePage: state => slug => {
+    return state.pages.find(page => page.slug === slug);
+    }
 };
